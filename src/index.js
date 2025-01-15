85fd1231a09da097f2732868d56ad318b80c7205
Liz Loomans <162842106+Liziloo@users.noreply.github.com>
2025-01-15 16:43:23 -0500

    Add invalid icon

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 import './styles/comeau-reset.css';
 import './styles/styles.css';
+import invalidIcon from './images/alpha-x-circle-outline.svg';
+
+const invalidDivs = document.querySelectorAll('.invalid-message');
+invalidDivs.forEach((div) => {
+    const icon = document.createElement('img');
+    icon.classList.add('invalid');
+    icon.src = invalidIcon;
+    div.prepend(icon);
+})
 
 const emailInput = document.querySelector('#email');
+emailInput.onfocusout = () => {
+    if (!emailInput.checkValidity()) {
+        
+    }
+}
 
 const countryInput = document.querySelector('#country');
 
