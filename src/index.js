de3d3aff2a77fec5f567d9f0bbe23c643b22c72a
Liz Loomans <162842106+Liziloo@users.noreply.github.com>
2025-01-15 16:00:36 -0500

    Create html form skeleton

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("Hello This is a Javascript project.");
+import './styles/comeau-reset.css';
+import './styles/styles.css';
+
+const emailInput = document.querySelector('#email');
+
+const countryInput = document.querySelector('#country');
+
+const zipInput = document.querySelector('#zip');
+
+const passInput = document.querySelector('#pass');
+
+const confirmInput = document.querySelector('#confirm');
+
