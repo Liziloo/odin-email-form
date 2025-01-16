7fc4ff1d5ae1f60a377a0b0c62d53aa524405cd6
Liz Loomans <162842106+Liziloo@users.noreply.github.com>
2025-01-16 10:09:28 -0500

    Add validity check for country

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import './styles/comeau-reset.css';
 import './styles/styles.css';
 import invalidIcon from './images/alpha-x-circle-outline.svg';
 
+const countries = ['United State', 'United Kingdom', 'Canada', 'Mexico'];
+
 const invalidDivs = document.querySelectorAll('.invalid-message');
 invalidDivs.forEach((div) => {
     const icon = document.createElement('img');
-    icon.classList.add('invalid');
+    icon.classList.add('invalid-icon');
     icon.src = invalidIcon;
     div.prepend(icon);
 })
@@ -14,13 +16,27 @@ const emailInput = document.querySelector('#email');
 emailInput.onblur = () => {
     const messageDiv = document.querySelector('.invalid-message.email');
     if (!emailInput.checkValidity()) {
-        messageDiv.style.display = 'flex';
+        messageDiv.classList.add('invalid');
     } else {
-        messageDiv.style.display = 'none';
+        messageDiv.classList.remove('invalid');
     }
 }
 
 const countryInput = document.querySelector('#country');
+for (let country of countries) {
+    const option = document.createElement('option');
+    option.value = country;
+    option.textContent = country;
+    countryInput.appendChild(option);
+}
+countryInput.onblur = () => {
+    const messageDiv = document.querySelector('.invalid-message.country');
+    if (!countries.includes(countryInput.value)) {
+        messageDiv.classList.add('invalid');
+    } else {
+        messageDiv.classList.remove('invalid');
+    }
+}
 
 const zipInput = document.querySelector('#zip');
 
